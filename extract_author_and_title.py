408d7a14bf6d768e57213376d1a393ff87062596
Michał Waszczuk <ihoold@gmail.com>
2018-05-20 20:15:36 +0200

    Script to make base feature list.
    Title, author are extracted from raw list.

---
+++ extract_author_and_title.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+data_path = '/home/ihoold/Downloads/Ksiazki'
+list_file_name = 'spis.csv'
+new_list_file_name = 'features.csv'
+
+if __name__ == '__main__':
+    os.chdir(data_path)
+    with open(list_file_name) as book_list:
+        with open(new_list_file_name, 'w') as new_list:
+            for line in book_list:
+                match = re.match(r',?,?\'?\"?(?P<name>.*) - (?P<authors>.*) ?\((?P<number>[0-9]*)\)\)?\'?\"?,?,?',
+                                 line, re.UNICODE)
+                if match:
+                    new_list.write(' '.join([match.group('number'), ';', match.group('name').replace(';', ''), ';',
+                                             match.group('authors').replace(';', ''), '\n']))
+                else:
+                    print(line.strip('\n'))
